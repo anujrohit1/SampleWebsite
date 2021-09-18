89b01545d71543bc1cb5e0080d1ef278a0eff2db
Anuj Kumar Rohit <39871559+anujrohit1@users.noreply.github.com>
2021-09-18 19:24:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from socket import gethostbyname, gethostname
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def home():
+    hostname = gethostname()
+    ipaddress = gethostbyname(hostname)
+    return render_template('index.html', hostname=hostname, ipaddress=ipaddress)
+
+
+if __name__ == '__main__':
+    app.run()
